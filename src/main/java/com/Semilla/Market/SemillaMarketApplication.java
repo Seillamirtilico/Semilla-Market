13ee013d14bdb71353413846664f1702012310b8
Julian <juliand.fuentesg@konradlorenz.edu.co>
2024-10-08 20:55:11 -0500

    En este punto ya llevamos el cascaron, los entitys y la conexion con la base de datos

---
+++ src/main/java/com/Semilla/Market/SemillaMarketApplication.java
@@ -0,0 +1,13 @@
+package com.Semilla.Market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class SemillaMarketApplication {
+
+	public static void main(String[] args) { SpringApplication.run(SemillaMarketApplication.class, args); }
+
+}
---
+++ src/test/java/com/Semilla/Market/SemillaMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Semilla.Market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemillaMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
